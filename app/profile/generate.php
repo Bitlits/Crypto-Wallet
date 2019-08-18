5cf3fc0a2eb1b90dbbf562d26d000a5cb5282e33
Justin Faler <jfaler216@student.msjc.edu>
2019-08-17 23:02:06 -0700

    Update generate.php

--- app/profile/generate.php
+++ app/profile/generate.php
@@ -17,33 +17,5 @@ $user = R::load("user",$id);
 
 include(APP."/profile/index.php");
 
-function checkCaptcha(){
-    if(isset($_POST['g-recaptcha-response'])){
-        $captcha = $_POST['g-recaptcha-response'];
-
-        $postdata = http_build_query(
-            array(
-                'secret'   => '	6LepdoEUAAAAAE5EOOXk8aNb4sCrn_YuwSxVGBn6',
-                'response' => $captcha,
-                'remoteip' => $_SERVER['REMOTE_ADDR']
-            )
-        );
-
-        $options = array('http' =>
-            array(
-                'method'  => 'POST',
-                'header'  => 'Content-type: application/x-www-form-urlencoded',
-                'content' => $postdata
-            )
-        );
-
-        $context = stream_context_create($options);
-        $result  = json_decode(file_get_contents('https://www.google.com/recaptcha/api/siteverify', false, $context));
-
-        return $result->success;
-    }else{
-        return false;
-    }
-}
 
 ?>
